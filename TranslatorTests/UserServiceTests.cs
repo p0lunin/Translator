49da5fecc2c5e5fa725f6652ae5aced638eaf2d8
p0lunin <dmytro.polunin@gmail.com>
2020-07-29 14:15:41 +0300

    added TestAuthNewUser, refactored tests

--- TranslatorTests/UserServiceTests.cs
+++ TranslatorTests/UserServiceTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
@@ -5,6 +6,7 @@ using NUnit.Framework;
 using Tranlator;
 using Tranlator.Models;
 using Tranlator.Services;
+using Tranlator.ViewModels;
 using TranslatorTests.Infrastructure;
 
 namespace TranslatorTests
@@ -15,41 +17,59 @@ namespace TranslatorTests
         private MemoryAuthLinksRepository _authLinksRepository;
         private OptionsWrapper<Settings> _options;
         private OneValueRandomGeneratorService<string> _randomGeneratorService;
+        private const string FakeMail = "fake@gmail.com";
+        private const string RandomValue = "123";
         [SetUp]
         public void Setup()
         {
             _userRepository = new MemoryUserRepository();
             _authLinksRepository = new MemoryAuthLinksRepository();
-            _randomGeneratorService = new OneValueRandomGeneratorService<string>("123");
+            _randomGeneratorService = new OneValueRandomGeneratorService<string>(RandomValue);
             _options = new OptionsWrapper<Settings>(new Settings("host"));
         }
 
         [Test]
-        public void TestSendAuthLink()
+        public async Task TestSendAuthLink()
         {
-            Task.Run(async () =>
-            {
-                var userService = new UserService(
-                    _randomGeneratorService, 
-                    _userRepository,
-                    _authLinksRepository,
-                    new MokeSmtpService((email, message) =>
-                    {
-                        Assert.AreEqual(email, "fake@mail.com");
-                        Assert.AreEqual(message, "host/users/auth?key=123");
-                    }),
-                    _options
-                    );
-                await userService.SendAuthLink("fake@mail.com");
-                var expected = new AuthLink
+            var userService = new UserService(
+                _randomGeneratorService, 
+                _userRepository,
+                _authLinksRepository,
+                new MokeSmtpService((email, message) =>
                 {
-                    Email = "fake@mail.com",
-                    Link = "123",
-                };
-                var actual = _authLinksRepository._links.First();
-                Assert.AreEqual(expected.Email, actual.Email);
-                Assert.AreEqual(expected.Link, actual.Link);
-            });
+                    Assert.AreEqual(email, FakeMail);
+                    Assert.AreEqual(message, $"host/users/auth?key={RandomValue}");
+                }),
+                _options
+                );
+            await userService.SendAuthLink(FakeMail);
+            var expected = new AuthLink
+            {
+                Email = FakeMail,
+                Link = RandomValue,
+            };
+            var actual = _authLinksRepository._links.First();
+            Assert.AreEqual(expected.Email, actual.Email);
+            Assert.AreEqual(expected.Link, actual.Link);
+        }
+        
+        [Test]
+        public async Task TestAuthNewUser()
+        {
+            var userService = new UserService(
+                _randomGeneratorService, 
+                _userRepository,
+                _authLinksRepository,
+                new MokeSmtpService((email, message) =>
+                {
+                    
+                }),
+                _options
+            );
+            await userService.SendAuthLink(FakeMail);
+            var actual = await userService.AuthUser(RandomValue);
+            var expected = new UserAuthorizedResult(FakeMail, true);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
