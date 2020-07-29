11825ffa259d55949758a4a7df9f4a48017a6fc6
p0lunin <dmytro.polunin@gmail.com>
2020-07-29 14:14:42 +0300

    generated Equals() and ToString() methods for UserAuthorizedResult

--- Tranlator/ViewModels/UserAuthorizedResult.cs
+++ Tranlator/ViewModels/UserAuthorizedResult.cs
@@ -1,4 +1,6 @@
-﻿namespace Tranlator.ViewModels
+﻿using System;
+
+namespace Tranlator.ViewModels
 {
     public class UserAuthorizedResult
     {
@@ -8,6 +10,28 @@
         {
             IsNewUser = isNewUser;
             Username = username;
+        }
+
+        protected bool Equals(UserAuthorizedResult other)
+        {
+            return IsNewUser == other.IsNewUser && Username == other.Username;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((UserAuthorizedResult) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(IsNewUser, Username);
+        }
+        public override string ToString()
+        {
+            return $"{nameof(IsNewUser)}: {IsNewUser}, {nameof(Username)}: {Username}";
         }
     }
 }
