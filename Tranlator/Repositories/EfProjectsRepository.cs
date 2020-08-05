9015de32b70558141047098499fa284dc2475a13
p0lunin <dmytro.polunin@gmail.com>
2020-08-05 18:44:15 +0300

    added IProjectRepository, EfProjectRepository

---
+++ Tranlator/Repositories/EfProjectsRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Tranlator.Models;
+
+namespace Tranlator.Repositories
+{
+    public class EfProjectsRepository : IProjectsRepository
+    {
+        private TranslatorContext _ctx;
+
+        public EfProjectsRepository(TranslatorContext ctx)
+        {
+            _ctx = ctx;
+        }
+        
+        public async Task SaveChanges()
+        {
+            await _ctx.SaveChangesAsync();
+        }
+
+        public async Task<Project> CreateProject(User owner, string name)
+        {
+            var project = new Project { Owner = owner, Name = name, IsPublic = false };
+            await _ctx.AddAsync(project);
+            return project;
+        }
+
+        public async Task<List<Project>> GetUserProjects(string username)
+        {
+            return await _ctx.Projects.Where(proj => proj.Owner.Name.Equals(username)).ToListAsync();
+        }
+    }
+}
---
+++ Tranlator/Repositories/IProjectsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Tranlator.Models;
+
+namespace Tranlator.Repositories
+{
+    public interface IProjectsRepository : IRepository
+    {
+        public Task<Project> CreateProject(User owner, string name);
+        public Task<List<Project>> GetUserProjects(string username);
+    }
+}
