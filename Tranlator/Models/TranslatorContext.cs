5f054e2ee0d3acf59b19827a31a0406549a64694
p0lunin <dmytro.polunin@gmail.com>
2020-08-05 12:11:57 +0300

    added methods for dotnet to work

--- Tranlator/Models/TranslatorContext.cs
+++ Tranlator/Models/TranslatorContext.cs
@@ -10,5 +10,15 @@ namespace Tranlator.Models
         public DbSet<Project> Projects { get; set; }
         public DbSet<AuthLink> LinksToAuth { get; set; }
         public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("Data Source=database.db");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
     }
 }
