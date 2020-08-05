0efe31a8ab789d9ff87db205e44bc5076c41de31
p0lunin <dmytro.polunin@gmail.com>
2020-08-05 11:35:08 +0300

    added tests for SmtpService

---
+++ TranslatorTests/Infrastructure/MokeSmtpClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Tranlator.Smtp;
+
+namespace TranslatorTests.Infrastructure
+{
+    public class MokeSmtpClient : ISmtpClient
+    {
+        public Action<MailMessage> OnSendMessage { get; }
+
+        public MokeSmtpClient(Action<MailMessage> onSendMessage)
+        {
+            OnSendMessage = onSendMessage;
+        }
+        public async Task SendMessage(MailMessage message)
+        {
+            OnSendMessage(message);
+        }
+    }
+}
---
+++ TranslatorTests/SmtpServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Mail;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using Tranlator;
+using Tranlator.Services;
+using TranslatorTests.Infrastructure;
+
+namespace TranslatorTests
+{
+    public class SmtpServiceTests
+    {
+        [Test]
+        public async Task TestSendMessage()
+        {
+            const string content = "content";
+            const string mail = "mail@mail.com";
+            var options = new OptionsWrapper<Settings>(new Settings("host"));
+            var mokeClient = new MokeSmtpClient(message =>
+            {
+                Assert.AreEqual(content, message.Body);
+                Assert.AreEqual(new MailAddress("noreply@host"), message.From);
+            });
+            var service = new SmtpService(options, mokeClient);
+
+            await service.SendMessage(mail, content);
+        }
+    }
+}
