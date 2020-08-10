07f861fab12a6cf916d1702334bbf5bda1609b09
p0lunin <dmytro.polunin@gmail.com>
2020-08-10 13:37:21 +0300

    added ProjectsService

--- Tranlator/Services/IProjectsService.cs
+++ Tranlator/Services/IProjectsService.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Tranlator.Models;
+using Tranlator.ViewModels;
 
 namespace Tranlator.Services
 {
     public interface IProjectsService
     {
-        public Task<Project> CreateNewProject(User owner, string name);
-        public Task<List<Project>> UserProjects(string username);
-        public Task UpdateParagraph(int paragraphId, string newContent);
+        public Task<ProjectInfoViewModel> CreateNewProject(User owner, string name);
+        public Task<List<ProjectInfoViewModel>> UserProjects(int userId);
+        public Task<List<LangInfoViewModel>> ProjectLangs(int projectId);
+        public Task<List<FileInfoViewModel>> LangFiles(int langId);
+        public Task<List<ParagraphInfoViewModel>> FileParagraphs(int fileId);
+        public Task UpdateParagraph(int userId, int paragraphId, string newContent);
     }
 }
--- Tranlator/Services/ProjectsService.cs
+++ Tranlator/Services/ProjectsService.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Tranlator.Exceptions;
 using Tranlator.Models;
 using Tranlator.Repositories;
+using Tranlator.ViewModels;
 
 namespace Tranlator.Services
 {
@@ -14,22 +17,64 @@ namespace Tranlator.Services
             _projectsRepository = projectsRepository;
         }
         
-        public async Task<Project> CreateNewProject(User owner, string name)
+        public async Task<ProjectInfoViewModel> CreateNewProject(User owner, string name)
         {
             var proj = await _projectsRepository.CreateProject(owner, name);
             await _projectsRepository.SaveChanges();
-            return proj;
+            return ProjectToViewModel(proj);
         }
 
-        public async Task<List<Project>> UserProjects(string username)
+        public async Task<List<ProjectInfoViewModel>> UserProjects(int userId)
         {
-            return await _projectsRepository.GetUserProjects(username);
+            return (await _projectsRepository.GetUserProjects(userId)).Select(ProjectToViewModel).ToList();
         }
 
-        public async Task UpdateParagraph(int paragraphId, string newContent)
+        public async Task<List<LangInfoViewModel>> ProjectLangs(int projectId)
         {
+            return (await _projectsRepository.GetProjectLangs(projectId)).Select(LangToViewModel).ToList();
+        }
+
+        public async Task<List<FileInfoViewModel>> LangFiles(int langId)
+        {
+            return (await _projectsRepository.GetLangFiles(langId)).Select(FileToViewModel).ToList();
+        }
+
+        public async Task<List<ParagraphInfoViewModel>> FileParagraphs(int fileId)
+        {
+            return (await _projectsRepository.GetFileParagraphs(fileId)).Select(ParagraphToViewModel).ToList();
+        }
+
+        public async Task UpdateParagraph(int userId, int paragraphId, string newContent)
+        {
+            await AssertUserIsAuthorOfParagraph(userId, paragraphId);
             await _projectsRepository.UpdateParagraph(paragraphId, newContent);
             await _projectsRepository.SaveChanges();
+        }
+
+        private async Task AssertUserIsAuthorOfParagraph(int userId, int paragraphId)
+        {
+            var author = await _projectsRepository.GetParagraphAuthor(paragraphId);
+            if (author.Id != userId)
+            {
+                throw new RecordNotFoundException("paragraph");
+            }
+        }
+
+        private static ProjectInfoViewModel ProjectToViewModel(Project project)
+        {
+            return new ProjectInfoViewModel(project.Id, project.Name, project.IsPublic);
+        }
+        private static LangInfoViewModel LangToViewModel(Lang lang)
+        {
+            return new LangInfoViewModel(lang.Id, lang.Name);
+        }
+        private static FileInfoViewModel FileToViewModel(File file)
+        {
+            return new FileInfoViewModel(file.Id, file.Name);
+        }
+        private static ParagraphInfoViewModel ParagraphToViewModel(Paragraph paragraph)
+        {
+            return new ParagraphInfoViewModel(paragraph.Id, paragraph.Order, paragraph.Content);
         }
     }
 }
